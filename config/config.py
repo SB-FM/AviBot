73c45ec7183a651a3b1b071976af103871c1c372
SB-FM <superbeast@email.com>
2017-09-18 21:10:22 +0200

    Removed all files that are in the .gitignore

--- config/config.py
+++
@@ -1,8 +0,0 @@
-from peewee import *
-
-server_id = '164072311161356289'
-# server_id = '337585332281409537'
-token = 'MzU2ODMwMDk4Nzc2Nzg0OTA3.DJhDeQ.hcADojSfXKjZy2PNIUnq8P1tNgw'
-
-db = SqliteDatabase(r'c:/Users/n_lan/PycharmProjects/AviBot/db/AviDB.db')
-db.connect()
